8848bc96cedbe9107af405aa0d0ce463fb420df4
Mikkel Malmberg <mikkel@brnbw.com>
2025-02-23 00:24:17 +0100

    Add option to show cheatsheet after delay

--- Leader Key/Defaults.swift
+++ Leader Key/Defaults.swift
@@ -15,12 +15,30 @@ extension Defaults.Keys {
     "forceEnglishKeyboardLayout", default: false, suite: defaultsSuite)
   static let modifierKeyForGroupSequence = Key<ModifierKey>(
     "modifierKeyForGroupSequence", default: .none, suite: defaultsSuite)
-  static let theme = Key<Theme>("theme", default: .mysteryBox, suite: defaultsSuite)
+  static let theme = Key<Theme>(
+    "theme", default: .mysteryBox, suite: defaultsSuite)
 
+  // Remove this in a few months
   static let alwaysShowCheatsheet = Key<Bool>(
     "alwaysShowCheatsheet", default: false, suite: defaultsSuite)
+
+  static let autoOpenCheatsheet = Key<AutoOpenCheatsheetSetting>(
+    "autoOpenCheatsheet", default: .delay, suite: defaultsSuite)
+  static let cheatsheetDelayMS = Key<Int>(
+    "cheatsheetDelayMS", default: 500, suite: defaultsSuite)
   static let expandGroupsInCheatsheet = Key<Bool>(
     "expandGroupsInCheatsheet", default: false, suite: defaultsSuite)
   static let showAppIconsInCheatsheet = Key<Bool>(
     "showAppIconsInCheatsheet", default: true, suite: defaultsSuite)
+}
+
+enum AutoOpenCheatsheetSetting: String, Defaults.Serializable {
+  case never
+  case always
+  case delay
+
+  // Remove this in a few months
+  static func defaultFromOldSetting(_ on: Bool) -> AutoOpenCheatsheetSetting {
+    return on ? .always : .delay
+  }
 }
